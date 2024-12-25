f7e7d1e972a41c602c40dac783f2a5dda9cb9336
Vlad Ivannikov <vladmaslow1@gmail.com>
2024-12-25 16:29:00 +0500

    add fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Hellow World')
+const getResourse = async (url) => {
+  const res = await fetch(url)
+  const body = res.json()
+  return body
+}
+
+getResourse('https://dog.ceo/api/breeds/image/random')
+  .then((body) => {
+    console.log(body)
+  })
