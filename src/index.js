0e97ebe6179a594c83e16622ecef486ff48f30ab
Vlad Ivannikov <vladmaslow1@gmail.com>
2024-12-25 16:36:18 +0500

    create new Error

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const getResourse = async (url) => {
   const res = await fetch(url)
+  if (!res.ok) {
+    throw new Error(`could not ${url}` + `, recived ${res.status}`) 
+  }
   const body = await res.json()
   return body
 }
 
-getResourse('https://dog.ceo/api/breeds/image/random')
+getResourse('https://dog.ceo/api/breeds/image/random/yerwer/fdsf')
   .then((body) => {
     console.log(body)
+  })
+  .catch((err) => {
+    console.log(err)
   })
